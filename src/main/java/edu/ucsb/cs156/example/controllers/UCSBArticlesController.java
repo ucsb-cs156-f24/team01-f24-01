78713048f58fe3b069de798e5a11e125396bd848
Raymond Chen <raymondchen@ucsb.edu>
2024-10-23 11:31:16 -0700

    Articles Controller

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBArticlesController.java
@@ -0,0 +1,154 @@
+package edu.ucsb.cs156.example.controllers;
+
+import edu.ucsb.cs156.example.entities.UCSBDate;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
+import edu.ucsb.cs156.example.repositories.UCSBDateRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+import java.time.LocalDateTime;
+
+/**
+ * This is a REST controller for UCSBArticles
+ */
+
+@Tag(name = "UCSBArticles")
+@RequestMapping("/api/ucsbarticles")
+@RestController
+@Slf4j
+public class UCSBArticlesController extends ApiController {
+
+    @Autowired
+    UCSBArticlesRepository ucsbArticlesRepository;
+
+    /**
+     * List all UCSB dates
+     * 
+     * @return an iterable of UCSBDate
+     */
+    @Operation(summary= "List all ucsb articles")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/all")
+    public Iterable<UCSBArticles> allUCSBArticles() {
+        Iterable<UCSBArticles> articles = ucsbArticlesRepository.findAll();
+        return articles;
+    }
+
+    /**
+     * Get a single date by id
+     * 
+     * @param id the id of the date
+     * @return a UCSBDate
+     */
+    @Operation(summary= "Get a single date")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public UCSBDate getById(
+            @Parameter(name="id") @RequestParam Long id) {
+        UCSBDate ucsbDate = ucsbDateRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(UCSBDate.class, id));
+
+        return ucsbDate;
+    }
+
+    /**
+     * Create a new date
+     * 
+     * @param title  the title of the article
+     * @param url          the url of the article
+     * @param explanation    a summary of the article
+     * @param email          an email address
+     * @param localDateTime the date
+     * @return the saved ucsbdate
+     */
+    @Operation(summary= "Create a new article")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/post")
+    public UCSBDate postUCSBArticles(
+            @Parameter(name="title") @RequestParam String title,
+            @Parameter(name="url") @RequestParam String url,
+            @Parameter(name="explanation") @RequestParam String explanation,
+            @Parameter(name="email") @RequestParam String email,
+            @Parameter(name="localDateTime", description="date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime localDateTime)
+            throws JsonProcessingException {
+
+        // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+        // See: https://www.baeldung.com/spring-date-parameters
+
+        log.info("localDateTime={}", localDateTime);
+
+        UCSBArticles ucsbArticles = new UCSBArticles();
+        ucsbArticles.setTitle(title);
+        ucsbArticles.setUrl(url);
+        ucsbArticles.setExplanation(explanation);
+        ucsbArticles.setEmail(email);
+        ucsbArticles.setLocalDateTime(localDateTime);
+
+        UCSBArticles savedUcsbArticles = ucsbArticlesRepository.save(ucsbArticles);
+
+        return savedUcsbArticles;
+    }
+
+    /**
+     * Delete a UCSBDate
+     * 
+     * @param id the id of the date to delete
+     * @return a message indicating the date was deleted
+     */
+    @Operation(summary= "Delete a UCSBDate")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("")
+    public Object deleteUCSBDate(
+            @Parameter(name="id") @RequestParam Long id) {
+        UCSBDate ucsbDate = ucsbDateRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(UCSBDate.class, id));
+
+        ucsbDateRepository.delete(ucsbDate);
+        return genericMessage("UCSBDate with id %s deleted".formatted(id));
+    }
+
+    /**
+     * Update a single date
+     * 
+     * @param id       id of the date to update
+     * @param incoming the new date
+     * @return the updated date object
+     */
+    @Operation(summary= "Update a single date")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public UCSBDate updateUCSBDate(
+            @Parameter(name="id") @RequestParam Long id,
+            @RequestBody @Valid UCSBDate incoming) {
+
+        UCSBDate ucsbDate = ucsbDateRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(UCSBDate.class, id));
+
+        ucsbDate.setQuarterYYYYQ(incoming.getQuarterYYYYQ());
+        ucsbDate.setName(incoming.getName());
+        ucsbDate.setLocalDateTime(incoming.getLocalDateTime());
+
+        ucsbDateRepository.save(ucsbDate);
+
+        return ucsbDate;
+    }
+}
